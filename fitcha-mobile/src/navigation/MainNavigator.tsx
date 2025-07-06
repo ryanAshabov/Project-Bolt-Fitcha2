18405a67d40c28bb19311f726f786eb68245f1c1
ryanAshabov <ryan.ashabov@gmail.com>
2025-07-06 23:09:27 +0300

    Updated MainNavigator.tsx

--- fitcha-mobile/src/navigation/MainNavigator.tsx
+++ fitcha-mobile/src/navigation/MainNavigator.tsx
@@ -1,47 +1,50 @@
-@@ .. @@
- import HomeScreen from '../screens/main/HomeScreen';
- import CourtsScreen from '../screens/main/CourtsScreen';
- import SmartFeaturesScreen from '../screens/main/SmartFeaturesScreen';
-+import MessagesScreen from '../screens/messages/MessagesScreen';
-+import ProfileScreen from '../screens/profile/ProfileScreen';
-+import SettingsScreen from '../screens/settings/SettingsScreen';
-+import NotificationsScreen from '../screens/notifications/NotificationsScreen';
- 
- export type MainStackParamList = {
-   Home: undefined;
-@@ .. @@
-   Courts: undefined;
-   CourtDetails: { courtId: string };
-   SmartFeatures: undefined;
-+  Messages: { conversationId?: string };
-+  Profile: { tab?: string };
-+  Settings: undefined;
-+  Notifications: undefined;
-+  OtherProfile: { userId: string };
-+  Friends: undefined;
-+  FindPartners: undefined;
-+  ChangePassword: undefined;
-+  PrivacySettings: undefined;
-+  HelpCenter: undefined;
-+  ContactUs: undefined;
-+  PrivacyPolicy: undefined;
-+  TermsOfService: undefined;
-+  DataUsage: undefined;
-+  Contacts: undefined;
-+  ARPreview: undefined;
-+  SmartBooking: undefined;
-+  GameDetails: { gameId: string };
- };
- 
- const Stack = createStackNavigator<MainStackParamList>();
-@@ .. @@
-       <Stack.Screen name="Home" component={HomeScreen} />
-       <Stack.Screen name="Courts" component={CourtsScreen} />
-       <Stack.Screen name="SmartFeatures" component={SmartFeaturesScreen} />
-+      <Stack.Screen name="Messages" component={MessagesScreen} />
-+      <Stack.Screen name="Profile" component={ProfileScreen} />
-+      <Stack.Screen name="Settings" component={SettingsScreen} />
-+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
-     </Stack.Navigator>
-   );
- };
+import HomeScreen from '../screens/main/HomeScreen';
+import CourtsScreen from '../screens/main/CourtsScreen';
+import SmartFeaturesScreen from '../screens/main/SmartFeaturesScreen';
+import MessagesScreen from '../screens/messages/MessagesScreen';
+import ProfileScreen from '../screens/profile/ProfileScreen';
+import SettingsScreen from '../screens/settings/SettingsScreen';
+import NotificationsScreen from '../screens/notifications/NotificationsScreen';
+
+export type MainStackParamList = {
+  Home: undefined;
+  Courts: undefined;
+  CourtDetails: { courtId: string };
+  SmartFeatures: undefined;
+  Messages: { conversationId?: string };
+  Profile: { tab?: string };
+  Settings: undefined;
+  Notifications: undefined;
+  OtherProfile: { userId: string };
+  Friends: undefined;
+  FindPartners: undefined;
+  ChangePassword: undefined;
+  PrivacySettings: undefined;
+  HelpCenter: undefined;
+  ContactUs: undefined;
+  PrivacyPolicy: undefined;
+  TermsOfService: undefined;
+  DataUsage: undefined;
+  Contacts: undefined;
+  ARPreview: undefined;
+  SmartBooking: undefined;
+  GameDetails: { gameId: string };
+};
+
+const Stack = createStackNavigator<MainStackParamList>();
+
+const MainStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Courts" component={CourtsScreen} />
+      <Stack.Screen name="SmartFeatures" component={SmartFeaturesScreen} />
+      <Stack.Screen name="Messages" component={MessagesScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainStack;
